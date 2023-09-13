24136e430e2237730d7e2ddc8f157ce22df5a05a
niseday <ikevinsama@gmail.com>
2023-09-13 23:59:10 +0800

    增加中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Student } from './entity/student';
-import { UserController } from './module/user/user.controller';
-import { UserService } from './module/user/user.service';
 import { UserModule } from './module/user/user.module';
 import { User } from './entity/user';
 import { StudentController } from './module/student/student.controller';
 import { StudentService } from './module/student/student.service';
 import { StudentModule } from './module/student/student.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [
@@ -20,14 +19,24 @@ import { StudentModule } from './module/student/student.module';
       username: 'root',
       password: '911520',
       database: 'test',
-      entities: [Student,User],
+      entities: [Student, User],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Student,User]),
+    TypeOrmModule.forFeature([Student, User]),
     UserModule,
     StudentModule, // 注册实体类
   ],
   controllers: [AppController, StudentController],
-  providers: [AppService, StudentService, ],
+  providers: [AppService, StudentService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware) //应用中间件
+      .exclude({ path: 'user', method: RequestMethod.POST }) //排除user的post方法
+      .forRoutes('user'); //监听路径  参数：路径名或*，*是匹配所以的路由
+    // .forRoutes({ path: 'user', method: RequestMethod.POST }, { path: 'album', method: RequestMethod.ALL }); //多个
+    // .apply(UserMiddleware) //支持多个中间件
+    // .forRoutes('user')
+  }
+}
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -5,57 +5,54 @@ import { User } from 'src/entity/user';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ) {}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-    async findAll(): Promise<User[]> {
-        return await this.userRepository.find();
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  //保存
+  async createUser(student: User): Promise<User> {
+    console.log(student);
+    return await this.userRepository.save(student);
+  }
+
+  //查找某一条
+  async findOne(id: number): Promise<User> {
+    return await this.userRepository.findOneBy({
+      userId: id,
+    });
+  }
+
+  //更新
+  async update(userData: User): Promise<User> {
+    await this.userRepository.update(userData.userId, userData);
+    return await this.userRepository.findOneBy({
+      userId: userData.userId,
+    });
+  }
+
+  //插入一条
+  async insertOne(userData: User): Promise<any> {
+    // let data = new Student();
+    // data.name = studentData.name;
+    // data.sex = studentData.sex;
+    // data.age = studentData.age;
+    await this.userRepository.insert(userData);
+    return await this.userRepository.findOneBy({
+      userId: userData.userId,
+    });
+  }
+
+  //删除
+  async delete(userData: User): Promise<any> {
+    try {
+      return await this.userRepository.remove(userData);
+    } catch (err) {
+      return err;
     }
-
-    //保存
-    async createUser(student: User): Promise<User> {
-        console.log(student)
-        return await this.userRepository.save(student);
-    }
-
-    //查找某一条
-    async findOne(id: number): Promise<User> {
-        return await this.userRepository.findOneBy({
-            userId: id
-        });
-    }
-
-    //更新
-    async update(userData: User): Promise<User> {
-
-        await this.userRepository.update(userData.userId, userData);
-        return await this.userRepository.findOneBy({
-            userId: userData.userId
-        });
-    }
-
-    //插入一条
-    async insertOne(userData: User): Promise<any> {
-        // let data = new Student();
-        // data.name = studentData.name;
-        // data.sex = studentData.sex;
-        // data.age = studentData.age;
-        await this.userRepository.insert(userData);
-        return await this.userRepository.findOneBy({
-            userId: userData.userId
-        });
-    }
-
-    //删除
-    async delete(userData: User): Promise<any> {
-        try {
-            return await this.userRepository.remove(userData);
-
-        } catch (err) {
-            return err
-        }
-
-    }
+  }
 }
